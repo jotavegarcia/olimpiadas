92c5e004411af37bd3e8fd82ce41c965a0daf99b
João Victor Garcia <130910563+jotavegarcia@users.noreply.github.com>
2024-09-07 10:06:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p> Ops... Nada foi encontrado! Digite o nome de algum atleta do Brasil<p> "
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let titulo = "";
+    let  descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+    }
+        // Concatena HTML para criar um novo elemento de resultado
+    if (!resultados ) {
+        resultados = "Ops... Nada foi encontrado!"
+    }
+
+    // Atribui os resultados formatados ao conteúdo da seção
+    section.innerHTML = resultados;
+}
+
---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao: "Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos poucos anos, ela já coleciona títulos e se tornou um exemplo de superação e determinação.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada fadinha sk8"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica artistica artística"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional. Com técnicas impecáveis e uma força física impressionante, ela é uma das grandes promessas do judô brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judoka"
+    } 
+];
+
